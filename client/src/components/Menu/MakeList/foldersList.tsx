3c815a748a826ad1574255fb3d121194e9a9bbe2
Daphne <daphne.kaihatsusha@gmail.com>
2022-01-07 08:57:51 +0100

    ♻️ (foldersList.tsx): Refacto functions getSubFolders & getDocuments + Fix "documents" variable use
    - Fusion et refactorisation des fonctions servant à récupérer la list des sous-dossiers et des documents en une seule fonction plus courte.
    - Reste le pb de types à fixer (ref ➔ l:35).
    
    - Fix de l’utilisations de la variables documents utilisé plusieurs fois avec un contenu différent 🙈 un faisant référence aux datas du fichier datas, l’autre aux documents présents dans le dossier courant. ➡️  Renomage de la seconde pour une meilleure compréhension.

--- client/src/components/Menu/MakeList/foldersList.tsx
+++ client/src/components/Menu/MakeList/foldersList.tsx
@@ -22,44 +22,47 @@ import {
 // ? Data Definition
 const { folders, documents } = data.notebook;
 
-// ? Functions Definition
-const getSubFolders: Function = (ids: string[]): FolderInterface[] | [] => {
-  const subFolders: FolderInterface[] = [];
-  ids.map((id: string) => {
-    folders.find((folder: FolderInterface) => {
-      if (folder.id === id) {
-        subFolders.push(folder);
-      }
-      return null;
-    });
-    return null;
-  });
-  return subFolders;
-};
+// ? Types Definition
+type listDatasType = FolderInterface[] | DocumentInterface[];
 
-const getDocuments: Function = (ids: string[]): DocumentInterface[] | [] => {
-  const docs: DocumentInterface[] = [];
-  ids.map((id: string) => {
-    documents.find((document: DocumentInterface) => {
-      if (document.id === id) {
-        docs.push(document);
-      }
-      return null;
-    });
-    return null;
+// ? Functions Definition
+/**
+ * @description This function is used to get the subFolders and documents datas in a secific folder.
+ * @param ids   String ➡️ The ids of the subFolder or Documents which we want.
+ * @param datas Array  ➡️ Array with all Folders or Documents datas from BDD.
+ * @returns     Array  ➡️ Array with the subFolders or Documents datas.
+ */
+// FIXME: How set `datas: listDatasType` (def l:26) without creating pb in `datas.find` with `d: FolderInterface | DocumentInterface`
+const getSubFoldersOrDocs: Function = (ids: string[], datas: []): listDatasType => {
+  const result: listDatasType = [];
+
+  ids.forEach((id: string) => {
+    const search = datas.find((d: FolderInterface | DocumentInterface ) => d.id === id);
+    return search && result.push(search);
   });
-  return docs;
+
+  return result;
 }
 
 const foldersList: Function = (list: FolderInterface[]): React.ReactElement[] => {
   const result: React.ReactElement[] = [];
+  
   list.forEach((folder: FolderInterface) => {
     const {
       id, name, childrensId, documentsId,
     } = folder;
-    const subFolders: FolderInterface[] | null = childrensId && childrensId.length > 0 ? getSubFolders(childrensId) : null;
-    const documents: DocumentInterface[] | null = documentsId && documentsId.length > 0 ? getDocuments(documentsId) : null;
-    console.log(name, documentsId, documents);
+
+    // FIXME: How set optional chaining => `childrensId?.length`
+    const subFolders: FolderInterface[] | null =
+      childrensId && childrensId.length > 0 
+        ? getSubFoldersOrDocs(childrensId, folders) 
+        : null;
+  
+    const docs: DocumentInterface[] | null =
+      documentsId && documentsId.length > 0 
+        ? getSubFoldersOrDocs(documentsId, documents) 
+        : null;
+    
     return result.push(
       <Accordion key={id}>
         <AccordionSummary>
@@ -69,7 +72,7 @@ const foldersList: Function = (list: FolderInterface[]): React.ReactElement[] =>
         </AccordionSummary>
         <AccordionDetails>
           {subFolders && foldersList(subFolders)}
-          {documents && documents.length > 0 && documentsList(documents)}
+          {docs && documentsList(docs)}
         </AccordionDetails>
       </Accordion>,
     );
