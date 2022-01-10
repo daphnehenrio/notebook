dbf0f7c4b11bd138577b38c9fcc4f83509f210d8
Daphne <daphne.kaihatsusha@gmail.com>
2022-01-10 07:41:54 +0100

    💄 (FolderIcon): Change icon style by folder empty or not

--- client/src/components/StyledComponents/Icons/index.tsx
+++ client/src/components/StyledComponents/Icons/index.tsx
@@ -2,14 +2,15 @@
 // ? Import NPM
 // | Material Icons
 import {
-  Folder,
+  FolderTwoTone,
 } from '@mui/icons-material';
 
 // | Material Styles
 import { styled } from '@mui/material/styles';
 
 // ? Export
-export const FolderIcon = styled(Folder)(() => ({
+export const FolderIcon = styled(FolderTwoTone)(({ isEmpty }: { isEmpty: boolean }) => ({
+  color: isEmpty ? 'grey' : 'black',
   marginRight: '3px',
   marginBottom: '2px',
 }));
